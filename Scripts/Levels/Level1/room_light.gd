6b7f0f79d1381666f46bdc2062a2b76a8809e8a2
Akio1997 <timothymcquin1997@gmail.com>
2026-07-25 19:45:57 +0800

    feat: add inital level 2

---
+++ Scripts/Levels/Level1/room_light.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+@onready var room_1: PointLight2D = $Room1
+@onready var room_2: PointLight2D = $Room2
+@onready var room_3: PointLight2D = $Room3
+@onready var room_4: PointLight2D = $Room4
+
+
+func _ready() -> void:
+	$Room1.color.a = 0
+	$Room2.color.a = 0
+	$Room3.color.a = 0
+	$Room4.color.a = 0
+	$Room5.color.a = 0
+
+
+func _on_r_1_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Room1.color.a = 1.0
+		print("HEY")
+
+
+func _on_r_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Room2.enabled = true
+		print("HEY")
+
+
+func _on_r_3_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Room3.enabled = true
+		print("HEY")
+
+
+func _on_r_4_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Room4.enabled = true
+		print("HEY")
+
+
+func _on_r_5_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Room5.enabled = true
+		print("HEY")
+
+
+func _on_r_1_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Room1.color.a = 0
+		print($Room1.enabled)
+
+
+func _on_r_2_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Room2.enabled = false
+		print("BYE")
+
+
+func _on_r_3_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Room3.enabled = false
+		print("BYE")
+
+
+func _on_r_4_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Room4.enabled = false
+		print("BYE")
+
+
+func _on_r_5_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Room5.enabled = false
+		print("BYE")
